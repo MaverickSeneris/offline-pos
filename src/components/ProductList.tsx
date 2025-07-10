7dd3405cd1003289f4bc0992f35990afd9dd94ce
Maverick Seneris <maverickseneris@gmail.com>
2025-07-10 13:39:46 +0800

    initial layout

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,23 @@
+import { products } from "../data/products";
+import type { Product } from "../data/products";
+
+type Props = {
+  onAddToCart: (product: Product) => void;
+};
+
+export default function ProductList({ onAddToCart }: Props) {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      {products.map((item) => (
+        <div
+          key={item.id}
+          className="bg-white p-4 rounded-lg shadow hover:bg-blue-100 cursor-pointer"
+          onClick={() => onAddToCart(item)}
+        >
+          <h2 className="font-semibold">{item.name}</h2>
+          <p className="text-sm text-gray-500">₱{item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
